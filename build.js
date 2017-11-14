ecfbf0afafe0b3d761036b9b4c93cece984f9787
31916088 <258220040@qq.com>
2017-11-14 19:55:15 +0800

    fix

--- build.js
+++ build.js
@@ -3,58 +3,63 @@ var fs = require('fs');
 var apiName = process.argv[2];
 
 var Build = {
-    create:function(){
+    create: function () {
+        this._init();
+        this.config.files.concat(
+            ['-rf',__dirname + '/mock', cwd],
+            [__dirname + '/src/page/index/index.js', cwd + '/src/page/index'],
+            [__dirname + '/src/page/index/html.js', cwd + '/src/page/index'],
+            [__dirname + '/src/page/index/gamejson.js', cwd + '/src/page/index'],
+            [__dirname + '/src/page/index/content.ejs', cwd + '/src/page/index'],
+            [__dirname + '/src/page/index/index.less', cwd + '/src/page/index'],
+            [__dirname + '/README.md', cwd],
+            [__dirname + '/.gitignore', cwd]
+        );
+        this.mkdir();
+        this.copy();
+    },
+
+    update: function () {
         this._init();
         this.mkdir();
         this.copy();
     },
 
-    update:function(){
-        this.create();
-    },
-
-    _init:function(){
+    _init: function () {
         var cwd = process.cwd();
         this.config = {
-            cwd:cwd,
-            files:[
-                ['-f',__dirname+'/gulpfile.js',cwd],
-                ['-f',__dirname+'/webpack.config.js',cwd],
-                ['-f',__dirname+'/package.json',cwd],
-                [__dirname+'/README.md',cwd],
-                [__dirname+'/.gitignore',cwd],
-                ['-rf',__dirname+'/mock',cwd],
-                ['-rf',__dirname+'/src/components',cwd+'/src'],
-                ['-rf',__dirname+'/src/modules',cwd+'/src'],
-                [__dirname+'/src/page/index/index.js',cwd+'/src/page/index'],
-                [__dirname+'/src/page/index/html.js',cwd+'/src/page/index'],
-                [__dirname+'/src/page/index/gamejson.js',cwd+'/src/page/index'],
-                [__dirname+'/src/page/index/content.ejs',cwd+'/src/page/index'],
-                [__dirname+'/src/page/index/index.less',cwd+'/src/page/index']
+            cwd: cwd,
+            files: [
+                ['-f', __dirname + '/gulpfile.js', cwd],
+                ['-f', __dirname + '/webpack.config.js', cwd],
+                ['-f', __dirname + '/package.json', cwd],
+                ['-rf', __dirname + '/src/components', cwd + '/src'],
+                ['-rf', __dirname + '/src/modules', cwd + '/src']
+
             ]
         };
     },
 
-    mkdir:function(){
-        var dirs = ['/src','/src/page','/src/page/index'];
+    mkdir: function () {
+        var dirs = ['/src', '/src/page', '/src/page/index'];
         var cwd = this.config.cwd;
-        dirs.forEach(function(dir){
-            dir = cwd+dir;
-            if(!fs.existsSync(dir)){
+        dirs.forEach(function (dir) {
+            dir = cwd + dir;
+            if (!fs.existsSync(dir)) {
                 fs.mkdirSync(dir);
             }
         });
     },
 
-    copy:function(){
+    copy: function () {
         var files = this.config.files;
-        files.forEach(function(file){
-            spawn('cp',file);
+        files.forEach(function (file) {
+            spawn('cp', file);
         })
     }
 };
 
-if(apiName && Build[apiName]){
+if (apiName && Build[apiName]) {
     Build[apiName]();
 }
 
