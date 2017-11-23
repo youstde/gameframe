1aa5ad4b6936a916f2bd41fc58624a65db1fc336
songtao <songtao_st08@163.com>
2017-11-23 17:09:01 +0800

    55

--- src/components/redpackage.js
+++ src/components/redpackage.js
@@ -1,5 +1,5 @@
 (function () {
-    var finallUlr = '//oss.ltcdn.cc/game/Theme/redpackage/prod/redpackage.js?btm=1123';
+    var finallUlr = '//oss.ltcdn.cc/game/Theme/redpackage/prod/redpackage.js?btm=112301';
     var scriptObj = document.createElement('script');
     scriptObj.src = finallUlr;
     document.getElementsByTagName("body")[0].appendChild(scriptObj);
