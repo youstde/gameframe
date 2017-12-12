62af5f2b7ce89dc31f813db3ab63babe3d9d88fc
songtao <songtao_st08@163.com>
2017-12-12 10:33:16 +0800

    28

--- src/components/buoy.js
+++ src/components/buoy.js
@@ -1,8 +1,9 @@
 ;(function (win) {
     win.BuoyMiddle = function (options) {
         var baseUrl = '//oss.ltcdn.cc/game/Theme/Buoy/Buoy',
-            finallUlr = '';
-        finallUlr = baseUrl + '_' + options.id + '.js?v=20171016111';
+            finallUlr = '',
+            BuoyId = Math.floor(Math.random() * 7 + 1);
+        finallUlr = baseUrl + '_' +BuoyId + '.js?v=20171016111';
         var scriptObj = document.createElement('script');
         scriptObj.src = finallUlr;
         document.getElementsByTagName("body")[0].appendChild(scriptObj);
