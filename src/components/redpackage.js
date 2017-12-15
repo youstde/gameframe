72cd46f83153bb2d0e6ab3ec3a2bb74571a91ec6
songtao <songtao_st08@163.com>
2017-12-15 13:50:20 +0800

    29

--- src/components/redpackage.js
+++ src/components/redpackage.js
@@ -1,5 +1,5 @@
 (function () {
-    var finallUlr = '//oss.ltcdn.cc/game/Theme/redpackage/prod/redpackage.js?btm=120602';
+    var finallUlr = '//oss.ltcdn.cc/game/Theme/redpackage/prod/redpackage.js?btm=121501';
     if(window.location.href.indexOf('pre')>-1){
         finallUlr = finallUlr.replace('prod','pre');
     }
