455ba22c30943da88df0a697ff83f01d1dc68ef5
songtao <songtao_st08@163.com>
2017-12-26 17:36:58 +0800

    39

--- src/components/layout/html.js
+++ src/components/layout/html.js
@@ -1,9 +1,10 @@
 var tpl = require('./html.ejs');
 var headTpl = require('./head.ejs');
+var baseJs = require('./base.ejs');
 
 var layout = function (content, pageTitle) {
     var config = {
-        headTpl:headTpl({pageTitle: pageTitle || '百泰游戏'}),
+        headTpl:headTpl({pageTitle: pageTitle || '百泰游戏',baseJs: baseJs()}),
         content: content || ''
     };
 
