ad9fe2a2b81362d73bd22844c4bd303bae5c18fa
ns <ns@nsdeMacBook-Pro.local>
2018-01-22 10:32:44 +0800

    fix

--- src/components/statement.js
+++ src/components/statement.js
@@ -6,9 +6,9 @@
         if(h2>h1){
             h=h2
         }
-        var text = '活动由来推互动主办，与Apple lnc.无关';
+        var text = '本活动与Apple lnc.无关';
         if(/android/i.test(window.navigator.userAgent)){
-            text = '本活动由：来推互动adbaitai.com提供';
+            text = '';
         }
         var el = document.createElement('div');
         el.style['text-align']='center';
@@ -18,7 +18,7 @@
         el.style.left=0;
         el.style.top=(h-23+'px');
         el.className='com-statement';
-        el.innerHTML='<a href="http://adbaitai.com" class="J_schema" onclick="_hmt.push([\'_trackEvent\', \'link\', \'click\', \'http://adbaitai.com\'])" data-id="statement" style="color: rgb(150, 200, 245); display: block;text-decoration: none;">'+text+'</a>';
+        el.innerHTML='<a href="javascript:;" class="J_schema" onclick="_hmt.push([\'_trackEvent\', \'link\', \'click\', \'http://adbaitai.com\'])" data-id="statement" style="color: rgb(150, 200, 245); display: block;text-decoration: none;">'+text+'</a>';
         setTimeout(function(){
             document.body.appendChild(el);
         },1000);
